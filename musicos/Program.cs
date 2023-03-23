d5e40ca5af2a7c8327699f7f1613b3aec5dd5e99
BrisaAguayo <124205663+BrisaAguayo@users.noreply.github.com>
2023-03-23 19:56:06 +0000

    Push

--- musicos/Program.cs
+++ musicos/Program.cs
@@ -8,10 +8,10 @@
     public Musico (string n) {Nombre = n;}
 
 
-    public void Saludar()
-    {
-        Console.WriteLine($"Hola soy {Nombre}");
-    }
+    public virtual string GetSaludo() => "Hola soy" + Nombre;
+
+    public virtual void Saludar() => Console.WriteLine(GetSaludo)
+    
 
     //para poder compartir en otro método se debe poner virtual
     public abstract void Tocar();
@@ -35,6 +35,10 @@ class Baterista:Musico
     {
         Console.WriteLine($"{Nombre} está tocando su {Bateria}");
     }
+
+    public override string GetSaludo() =>  base.GetSaludo() + " y soy baterista";
+     public override void Saludar() => Console.WriteLine(GetSaludo());
+   
 }
 
 class Bajista:Musico
@@ -53,6 +57,34 @@ class Bajista:Musico
     {
         Console.WriteLine($"{Nombre} está tocando su {Bajo}");
     }
+
+    public override void Saludar()
+    {
+        Console.WriteLine($"Hola soy {Nombre} y toco el {Bajo}");
+    }
+}
+
+class Voz:Musico
+{
+
+    //Constructor Bajo
+    public string voz {get;set;}
+
+    public  Voz (string n, string b):base(n)
+    {
+        voz = b;
+    }
+
+    //override refedine un método virtual
+    public override void Tocar()
+    {
+        Console.WriteLine($"El instrumentos de {Nombre} es su {voz}");
+    }
+
+    public override void Saludar()
+    {
+        Console.WriteLine($"Hola soy {Nombre} y mi instrumento es mi {voz}");
+    }
 }
 
 class Program
@@ -60,14 +92,16 @@ class Program
     private static void Main(string[] args)
     {
         List<Musico> banda = new List<Musico>();
+        banda.Add(new Voz("Wos", "Voz"));
         banda.Add(new Bajista("Natasha", "bajo"));
         banda.Add(new Baterista("Evlay", "Bateria"));
         foreach(var m in banda)
         {
+        Console.WriteLine("--------");
         m.Tocar();
+        m.Saludar();
         }       
 
-        Console.WriteLine(banda);
     }
 }
 
